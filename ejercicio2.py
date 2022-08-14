bcea53b9f06f84959810cb8c585b9568e0d72a48
mauriciojfranco <100097266+mauriciojfranco@users.noreply.github.com>
2022-08-13 21:09:45 -0300

    Ejercicio 2 de Valoración final Modulo Programador
    2)   Crear una lista en Python denominada “Dueno2”  y recorrerla con un bucle  mostrando sus elementos por pantalla con excepción del DNI y el apellido. Los elementos de la lista son:
    
               23546987,  “Maria”,  “Perez”, 4789689,  “Pueyrredon  811”

---
+++ ejercicio2.py
@@ -0,0 +1,16 @@
+
+#Dueno2 = [DNI, nombre, apellido, teléfono, dirección]
+
+Dueno2 = [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
+
+n= 0
+while n<len(Dueno2):
+    
+    if n !=0 and n!=2:
+        print(Dueno2[n])
+    n+=1
+    
+
+    
+
+
