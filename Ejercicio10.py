eb40118f6f9581b976f08a646e38ea201fef517b
ledesma cintia <cintial_cba@hotmail.com>
2022-08-14 21:15:23 -0300

    Ejercicio10

---
+++ Ejercicio10.py
@@ -0,0 +1,5 @@
+#Crear una lista denominada “Clientes” con los nombres de los diferentes  dueños de perros. Juan,  Mario,  Ariel,  Josefina,  Marianella.
+
+#Ordenarla alfabéticamente y mostrarla por pantalla.
+clientes=("Juan", "Mario", "Ariel", "Josefina", "Marianella")
+print(sorted(clientes))
